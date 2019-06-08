983ab5b43a735d9cfc7413698afacf596e5ec60c
ellamo <margaretemoore@gmail.com>
2019-06-08 17:31:44 -0400

    python solution for MinimumSwaps2 times out on large test cases --- same solution passes all tests in Java

--- Practice_InterviewPreparationKit_Arrays/MinimumSwaps2.py
+++ Practice_InterviewPreparationKit_Arrays/MinimumSwaps2.py
@@ -14,14 +14,17 @@ n = len(arr)
 
 
 # find start of consecutive numbers - this will be the array position offset
-offset = None
-for i in range(n-1):
-    if i == 0:
-        offset = arr[i]
-    elif arr[i] < offset:
-        offset = arr[i]
+# not necessary --- all sets start with 1
+#offset = None
+#for i in range(n-1):
+#    if i == 0:
+#        offset = arr[i]
+#    elif arr[i] < offset:
+#        offset = arr[i]
 
+### this solution times out for large test cases due to O(n^2) time of nested loop --- WORKS IN JAVA
 # execute swaps (from left to right switch out of order int with the correct int for that position) and count them
+offset = 1
 swapcount = 0
 for i in range(n):
     if arr[i] != i + offset:
@@ -29,7 +32,34 @@ for i in range(n):
             if arr[j] == i + offset:
                 arr[i], arr[j] = arr[j], arr[i]
                 swapcount += 1
-            
-print(swapcount)            
+                break         
+print(swapcount)      
+
+
+### this doesn't work - logical error
+#count = 0
+#for i in range(n):
+#    if (arr[i] != i + 1) and (arr[i] < arr[arr[i]-1]):
+#        arr[i], arr[arr[i] - 1] = arr[arr[i] - 1], arr[i]
+#        print("swapping", arr[i], " and ", arr[arr[i] -1])
+#        count += 1
+#
+#print(count)  
+
+
+### doesn't work, logical error
+#left = 0
+#right = len(arr) -1
+#minswaps = 0
+#while left < right:
+#    while (arr[left] == left + 1) and (left < right):
+#        left += 1
+#    if (left < right) :
+#       arr[i], arr[arr[i] - 1] = arr[arr[i] - 1], arr[i]
+#       minswaps += 1
+#    left += 1
+#   
+#print(minswaps)
+
 
         
